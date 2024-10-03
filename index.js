6c2a6dd62a9ff11844ec8a1b527b44b735331b21
damarismuthoni <muthonidamah@gmail.com>
2024-10-03 17:13:23 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const africastalking = require('africastalking')({
+    apiKey: 'YOUR_API_KEY',
+    username: 'YOUR_USERNAME'
+  });
+  
+  const sms = africastalking.SMS;
+  const voice = africastalking.VOICE;
+  const ussd = africastalking.USSD;
+  
